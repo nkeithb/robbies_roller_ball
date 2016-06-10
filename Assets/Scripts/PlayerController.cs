a9539c05755c19d2b7365239a3630ba243235b99
Robierto <robert_sebree@yahoo.com>
2016-06-10 18:30:44 -0400

    Initital push.#

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    // Sets Rigidbody name to rb in script
+    private Rigidbody rb;
+    // Sets rb to get the component Rigidbody
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Checks for input from keyboard to determine Horizontal and Vertical movement of "Player"
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    //Calls to rb to add force to the player object according to the (X,Y,Z) Vector Coordinates
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+}

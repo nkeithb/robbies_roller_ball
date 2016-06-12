8647fd7ec5abb1083b660927fa4402344cafac97
Robierto <robert_sebree@yahoo.com>
2016-06-12 18:38:11 -0400

    Full Update

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	void Update () {
+
+        transform.Rotate (new Vector3 (90,45,15) * Time.deltaTime);
+	}
+}

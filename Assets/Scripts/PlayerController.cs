ebc27c79e2985e4910cfa75d283d7a20a6a07b51
Robierto <robert_sebree@yahoo.com>
2016-06-12 03:06:53 -0400

    Finish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
-    // Sets Rigidbody name to rb in script
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
-    // Sets rb to get the component Rigidbody
-    void Start ()
+    private int count;
+    //Sets base information for all Variables at the start of the run.
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     // Checks for input from keyboard to determine Horizontal and Vertical movement of "Player"
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-    //Calls to rb to add force to the player object according to the (X,Y,Z) Vector Coordinates
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
 
-        rb.AddForce (movement * speed);
+    //Checks for collision with "pick up" 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    //Sets the counting and win text UI display in real time.
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 8)
+        {
+            winText.text = "YOU WIN!";
+        }
     }
 }

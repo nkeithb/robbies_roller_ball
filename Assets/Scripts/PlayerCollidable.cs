db0e3816828db95f46e6faf03a6684912b111a87
nathan <nburing@norampartners.com>
2016-06-13 01:01:03 -0400

    Changed if statements to case. FUCKING INHERITENCE!

---
+++ Assets/Scripts/PlayerCollidable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollidable : MonoBehaviour {
+
+    public bool collisionResult;
+    public int collisionReturn;
+
+    public int HitByPlayer()
+    {
+        this.gameObject.SetActive(collisionResult);
+        return this.collisionReturn;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,24 +29,23 @@ public class PlayerController : MonoBehaviour {
     //Checks for collision with "pick up" 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        switch (other.tag)
         {
-            other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
+            case "Pick Up":
+                other.gameObject.SetActive(false);
+                count++;
+                SetCountText();
+                break;
+            case "DontPickUp":
+                other.gameObject.SetActive(false);
+                count--;
+                SetCountText();
+                break;
+            case "AntiPlayer":
+                count--;
+                SetCountText();
+                break;
         }
-        else if (other.gameObject.CompareTag("DontPickUp"))
-        {
-            other.gameObject.SetActive(false);
-            count--;
-            SetCountText();
-        }
-        else if (other.gameObject.CompareTag("AntiPlayer"))
-        {
-            count--;
-            SetCountText();
-        }
-    //Sets the counting and win text UI display in real time.
     }
     void SetCountText()
     {

7adb568a37aa79a844ee873000e6375f4294ea11
Robierto <robert_sebree@yahoo.com>
2016-06-14 02:21:42 -0400

    gty

--- Assets/Scripts/AnitPlayerController.cs
+++ Assets/Scripts/AnitPlayerController.cs
@@ -17,15 +17,12 @@ public class AnitPlayerController : MonoBehaviour {
         if (target != null)
         {
             Vector3 dir = target.position - transform.position;
-            // Only needed if objects don't share 'z' value.
             dir.z = 0.0f;
             if (dir != Vector3.zero)
                 transform.rotation = Quaternion.Slerp(transform.rotation,
                     Quaternion.FromToRotation(Vector3.right, dir),
                     rotationSpeed * Time.deltaTime);
-
-            //Move Towards Target
-            transform.position += (target.position - transform.position).normalized
+                 transform.position += (target.position - transform.position).normalized
                 * moveSpeed * Time.deltaTime;
         }
     }

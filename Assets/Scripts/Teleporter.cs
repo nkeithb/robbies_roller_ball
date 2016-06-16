526379c75ef1444a846c83669416a6e19767327b
Tom <hainest1@nku.edu>
2016-06-16 10:53:14 -0400

    Teleporter script improved

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -6,8 +6,8 @@ public class Teleporter : MonoBehaviour {
     // This script is currently unused, however teleporter functionality may be able to be moved in to this class
     // It could also be changed to accept a public GameObject in-editor as the destination to add functionality
 
-
-    public float teleportDelay;
+    public GameObject destination;
+    public float teleportDelay = 0.1f;
     private bool recentlyTeleported = false;
     private Transform playerTransform;
     private Transform spawnPoint;
@@ -28,10 +28,10 @@ public class Teleporter : MonoBehaviour {
         // Here, 'Teleporter1' will hold tag 'Teleporter2' and vice versa, creating the teleport effect. 
         // Can be used multiple times, however the tag of the teleporter MUST match the name of the 
         // target teleporter.
-        if (other.name.Contains("Teleporter") && recentlyTeleported == false)
+        if (other.name.Contains("Player") && recentlyTeleported == false)
         {
-            //SoundManager.instance.RandomizeSfx(teleportSounds);
-            Teleport(other.tag);
+            SoundManager.instance.RandomizeSfx(PlayerController.instance.teleportSounds);
+            Teleport(destination.tag);
             AddTeleportDelay();
         }
     }
@@ -50,9 +50,9 @@ public class Teleporter : MonoBehaviour {
     private void Teleport(string spawnTag)
     {
         SetTransformValues(spawnTag);
+        playerTransform.position = spawnPoint.position;
         rigidBody.velocity = new Vector3(0, 0, 0);
         rigidBody.ResetInertiaTensor();
-        playerTransform.position = spawnPoint.position;
     }
 
     private void SetTransformValues(string spawnTag)
